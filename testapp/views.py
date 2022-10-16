03d751806d1b97921d0f1ffeb4b356cd3e211791
pushpaziphio <pushpa.k@dsp.com.sg>
2022-10-16 20:20:28 +0530

    learning

---
+++ testapp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+# request -> response
+# request handler
+# action
+
+
+def say_hello(request):
+    # return HttpResponse('Hello World')
+    return render(request, 'hello.html', {'name': 'Pushpa'})
